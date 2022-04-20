4cfab0bb51237353f483062c8e105b8b48f1f72b
ParkSunA <psa199402@gmail.com>
2022-04-20 23:48:32 +0900

    애플리케이션의 전체 동작 방식을 구성하는 AppConfig 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
     // 이 코드의 문제점은 MemberServiceImpl이 MemberRepository(추상화)에도 의존하고 MemoryMemberRepository(구체화)에도 의존
     // DIP를 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
